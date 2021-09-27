9b4f2576ed69d189f8edf8ff5afd1c036d217402
Ali Nahid <alinahid@gmail.com>
2021-09-27 13:26:58 +1000

    bugfix

--- converter.ps1
+++ converter.ps1
@@ -1,13 +1,12 @@
-Get-ChildItem -Recurse -Filter * | Where-Object {$_.PsIsContainer -eq $True -and ($_.Name -notlike "images*" -and $_.Name -notlike ".git*") } | 
+Get-ChildItem -Recurse -Filter * | Where-Object {$_.PsIsContainer -eq $True -and ($_.Name -NotLike ".git*" -and $_.Name -NotLike "images*") } | 
 Foreach-Object { 
 	$folder=$_.FullName
 	Write-Output "processing dir ... $folder"
 
-	Get-ChildItem $folder -Filter * |
+	Get-ChildItem $folder\* -Include *.sh, *.yaml |
 		Foreach-Object {
-			Write-Output "processing $_..."
-
 			$original_file =$_.FullName
+			Write-Output "processing $original_file..."
 			$text = [IO.File]::ReadAllText($original_file) -replace "`r`n", "`n"
 			[IO.File]::WriteAllText($original_file, $text)
 		}
